31e0f0a5fe6e2c8288aa9dfd25ded05bd07ad4f7
RaftDev <theraft08@gmail.com>
2026-03-31 22:28:07 +0200

    Refactor: Reworked a lot of the current code, and added the LUT

--- src/main.ino
+++ src/main.ino
@@ -6,59 +6,159 @@
 #define SENSOR_3 35
 #define SENSOR_4 21
 
-#define UNDEFINED_READ 60
+#define MOTOR_RIGHT 16
+#define MOTOR_LEFT 17
+#define INDICATOR 5
+
+#define SENSOR_COUNT 5
+#define HISTORY_SIZE 10
+#define MAX_VALUE 31
+
+// Makes sure all values are way over the maximum possible value of the combined sensor
+#define UNDEFINED_READ MAX_VALUE + 10
 
 int readIndex = 0;
-int history[10]; // Put value read by captor every 20ms and rewrite the values if a value is written
+int history[HISTORY_SIZE];
+int freq[MAX_VALUE];
 
-struct Instructions {
-    int left;
-    int right;
+int lastReads[SENSOR_COUNT];
+
+struct instruction {
+    int rightSpeed;
+    int leftSpeed;
 };
 
+struct instruction LUT[] = {
+    {0, 0 }, // 0  - 0 0 0 0 0 - No Sensor active
+    {}, // 1  - 0 0 0 0 1 -
+    {}, // 2  - 0 0 0 1 0 -
+    {}, // 3  - 0 0 0 1 1 -
+    {}, // 4  - 0 0 1 0 0 - Center
+    {}, // 5  - 0 0 1 0 1 -
+    {}, // 6  - 0 0 1 1 0 -
+    {}, // 7  - 0 0 1 1 1 -
+    {}, // 8  - 0 1 0 0 0 -
+    {}, // 9  - 0 1 0 0 1 -
+    {}, // 10 - 0 1 0 1 0 -
+    {}, // 11 - 0 1 0 1 1 -
+    {}, // 12 - 0 1 1 0 0 -
+    {}, // 13 - 0 1 1 0 1 -
+    {}, // 14 - 0 1 1 1 0 - Usual center
+    {}, // 15 - 0 1 1 1 1 -
+    {}, // 16 - 1 0 0 0 0 -
+    {}, // 17 - 1 0 0 0 1 -
+    {}, // 18 - 1 0 0 1 0 -
+    {}, // 19 - 1 0 0 1 1 -
+    {}, // 20 - 1 0 1 0 0 -
+    {}, // 21 - 1 0 1 0 1 -
+    {}, // 22 - 1 0 1 1 0 -
+    {}, // 23 - 1 0 1 1 1 -
+    {}, // 24 - 1 1 0 0 0 -
+    {}, // 25 - 1 1 0 0 1 -
+    {}, // 26 - 1 1 0 1 0 -
+    {}, // 27 - 1 1 0 1 1 -
+    {}, // 28 - 1 1 1 0 0 -
+    {}, // 29 - 1 1 1 0 1 -
+    {}, // 30 - 1 1 1 1 0 -
+    {}, // 31 - 1 1 1 1 1 - All Sensors active
+};
 
 void setup() {
     M5.begin();
-
     readIndex = 0;
 
-    for (int i = 0; i < 10; i++) {
-        history[i] = UNDEFINED_READ; // Makes sure all values are at 0
+    for (int i = 0; i < HISTORY_SIZE; i++) {
+        // Set all the history to the impossible value to make sure we can differentiate undefined values from actual values
+        history[i] = UNDEFINED_READ;
     }
 
+    for (int i = 0; i < MAX_VALUE; i++) {
+        freq[i] = 0;
+    }
+
+    // Define pins
     pinMode(SENSOR_0, INPUT);
     pinMode(SENSOR_1, INPUT);
     pinMode(SENSOR_2, INPUT);
     pinMode(SENSOR_3, INPUT);
     pinMode(SENSOR_4, INPUT);
 
+    pinMode(MOTOR_RIGHT, OUTPUT);
+    pinMode(MOTOR_LEFT, OUTPUT);
+    pinMode(INDICATOR, OUTPUT);
+
     M5.Lcd.setTextSize(2);
 }
 
-void loop() {
-    int cap0 = digitalRead(SENSOR_0); // Ok 1
-    int cap1 = digitalRead(SENSOR_1); // Ok 2
-    int cap2 = digitalRead(SENSOR_2); // Ok 4
-    int cap3 = digitalRead(SENSOR_3); // OK 8
-    int cap4 = digitalRead(SENSOR_4); // Ok 16
+int transformInput() {
+    int value = 0;
 
-    int result = trad(cap4, cap3, cap2, cap1, cap0);
+    for (int i = 0; i < SENSOR_COUNT; i++) {
+        value += lastReads[i] << i;
+    }
 
-    M5.Lcd.setCursor(10, 10);
-    M5.Lcd.clear();
-
-    readIndex++;
-    M5.Lcd.printf("%d: %d; ", readIndex, result);
-
-    delay(20);
-
-    // For one loop put just one value in the history, for the second loop the second one, the third loop... 10 times and then rewrite not replace all the values (this code replaces all the values) with 20 ms
-    // M5.Lcd.print(result);
-
-    delay(500);
+    return value;
 }
 
+void updateIndex() {
+    if (readIndex < HISTORY_SIZE) {
+        readIndex++;
+    } else {
+        readIndex = 0;
+    }
+}
 
-int trad(int cap4, int cap3, int cap2, int cap1, int cap0) {
-    return (cap4 * 16) + (cap3 * 8) + (cap2 * 4) + (cap1 * 2) + (cap0);
+void debugDisplay() {
+    M5.Lcd.clear();
+
+    int currentValue = history[readIndex];
+
+    M5.Lcd.setCursor(20, 30); M5.Lcd.printf("L2:%1d L1:%1d C:%1d R1:%1d R2:%1d \n",digitalRead(IR_L2),digitalRead(IR_L1),digitalRead(IR_C),digitalRead(IR_R1),digitalRead(IR_R2));
+
+    M5.Lcd.setCursor(10, 10);
+    M5.Lcd.printf("Read Index: %d", readIndex);
+}
+
+void move(int left, int right) {
+    // TODO: Implement motors
+}
+
+void loop() {
+    updateIndex();
+
+    lastReads[0] = !digitalRead(SENSOR_0); // ^ 1
+    lastReads[1] = !digitalRead(SENSOR_1); // ^ 2
+    lastReads[2] = !digitalRead(SENSOR_2); // ^ 4
+    lastReads[3] = !digitalRead(SENSOR_3); // ^ 8
+    lastReads[4] = !digitalRead(SENSOR_4); // ^ 16
+
+    int value = transformInput();
+
+    // Handle History and frequency table
+    int previousValue = history[readIndex];
+    if (previousValue != UNDEFINED_READ)
+        freq[previousValue]--;
+
+    history[readIndex] = value; // Write to the history
+    freq[value]++;
+
+    int max = 0;
+    int actionValue = 0;
+    for (int i = 0; i < MAX_VALUE; i++) {
+        if (freq[i] > max) {
+            max = freq[i];
+            actionValue = i;
+        }
+    }
+
+    instruction action = LUT[actionValue];
+
+    M5.Lcd.clear();
+    M5.Lcd.setCursor(10, 10);
+    M5.Lcd.printf("%d: %d; ", readIndex, value);
+
+    debugDisplay();
+
+    move(action.leftSpeed, action.rightSpeed);
+    delay(250); // ~4 reads/sec
 }
