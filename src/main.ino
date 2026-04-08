24b7f8d19446fe75fd049a62fbce49a6e7906a02
RaftDev <theraft08@gmail.com>
2026-04-08 12:53:01 +0200

    refactor: Full rewrite

--- src/main.ino
+++ src/main.ino
@@ -14,7 +14,7 @@
 #define HISTORY_SIZE 10
 #define MAX_VALUE 31
 
-#define BASE_SPEED 6
+#define BASE_SPEED 100
 
 // LEDC
 #define MOTOR_RIGHT_CHANNEL 1
@@ -32,54 +32,6 @@ int history[HISTORY_SIZE];
 int freq[MAX_VALUE + 1];
 
 int lastReads[SENSOR_COUNT];
-
-struct instruction {
-    byte leftSpeed;
-    byte rightSpeed;
-};
-
-instruction LUT[] = {
-    {6, 6},   // 0  - 0 0 0 0 0 - All Sensors active (intersection)
-
-    {10, 3},   // 1  - 0 0 0 0 1 -
-    {10, 8},   // 2  - 0 0 0 1 0 -
-    {10, 6},   // 3  - 0 0 0 1 1 -
-
-    {10,10},  // 4  - 0 0 1 0 0 - Center
-    {9, 10},   // 5  - 0 0 1 0 1 -
-    {10, 8},   // 6  - 0 0 1 1 0 -
-    {10, 6},   // 7  - 0 0 1 1 1 -
-
-    {8, 10},   // 8  - 0 1 0 0 0 -
-    {8, 10},    // 9  - 0 1 0 0 1 -
-    {8, 10},   // 10 - 0 1 0 1 0 -
-    {10, 8},    // 11 - 0 1 0 1 1 -
-
-    {8, 10},   // 12 - 0 1 1 0 0 -
-    {8, 10},    // 13 - 0 1 1 0 1 -
-    {10, 10},  // 14 - 0 1 1 1 0 - Usual center
-    {10, 9},    // 15 - 0 1 1 1 1 -
-
-    {3 ,10},   // 16 - 1 0 0 0 0 -
-    {3, 10},   // 17 - 1 0 0 0 1 -
-    {10, 8},   // 18 - 1 0 0 1 0 -
-    {10, 8},   // 19 - 1 0 0 1 1 -
-
-    {10, 10},   // 20 - 1 0 1 0 0 -
-    {10, 10},    // 21 - 1 0 1 0 1 -
-    {10, 9},   // 22 - 1 0 1 1 0 -
-    {10, 8},    // 23 - 1 0 1 1 1 -
-
-    {8, 10},   // 24 - 1 1 0 0 0 -
-    {8, 10},    // 25 - 1 1 0 0 1 -
-    {8, 10},   // 26 - 1 1 0 1 0 -
-    {8, 10},    // 27 - 1 1 0 1 1 -
-
-    {6, 10},   // 28 - 1 1 1 0 0 -
-    {8, 10},    // 29 - 1 1 1 0 1 -
-    {9, 10},  // 30 - 1 1 1 1 0 -
-    {0,0}     // 31 - 1 1 1 1 1 - No sensors
-};
 
 void setup() {
     M5.begin();
@@ -166,40 +118,16 @@ void move(int left, int right) {
 }
 
 void loop() {
-    lastReads[0] = !digitalRead(SENSOR_0); // ^ 1
-    lastReads[1] = !digitalRead(SENSOR_1); // ^ 2
-    lastReads[2] = !digitalRead(SENSOR_2); // ^ 4
-    lastReads[3] = !digitalRead(SENSOR_3); // ^ 8
-    lastReads[4] = !digitalRead(SENSOR_4); // ^ 16
+    int err = 0;
 
-    int value = transformInput();
+    err += digitalRead(SENSOR_0) ? -2 : 0;
+    err += digitalRead(SENSOR_1) ? -1 : 0;
+    err += digitalRead(SENSOR_3) ? 1 : 0;
+    err += digitalRead(SENSOR_4) ? 2 : 0;
 
-    // Handle History and frequency table
-    /*
-    int previousValue = history[readIndex];
-    if (previousValue != UNDEFINED_READ)
-        freq[previousValue]--;
+    int speedLeft = BASE_SPEED + err * 2;
+    int speedRight = BASE_SPEED - err * 2;
 
-    history[readIndex] = value; // Write to the history
-
-    freq[value]++;
-
-    int max = 0;
-    int actionValue = 0;
-    for (int i = 0; i < MAX_VALUE; i++) {
-        if (freq[i] > max) {
-            max = freq[i];
-            actionValue = i;
-        }
-    }*/
-
-    history[readIndex] = value; // Write to the history
-    instruction action = LUT[value];
-
-    debugDisplay(action.leftSpeed * BASE_SPEED, action.rightSpeed * BASE_SPEED);
-
-    move(action.leftSpeed * BASE_SPEED, action.rightSpeed * BASE_SPEED);
-
-    updateIndex();
-    //delay(2); // ~2 reads/sec
+    debugDisplay(speedLeft, speedRight);
+    move(70, 70);
 }
