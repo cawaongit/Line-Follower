ee828fb3975c77a2989377e501314d5ac4b58827
RaftDev <theraft08@gmail.com>
2026-04-18 01:28:20 +0200

    refactor: Reworked the whole algorithm once again

--- src/main.ino
+++ src/main.ino
@@ -1,14 +1,14 @@
 #include <M5Stack.h>
 
-#define SENSOR_0 21
-#define SENSOR_1 35
-#define SENSOR_2 36
-#define SENSOR_3 13
-#define SENSOR_4 34
+#define IR_L2 34
+#define IR_L1 13
+#define IR_C 36
+#define IR_R1 35
+#define IR_R2 21
 
-#define BTN_A 39
-#define BTN_B 38
-#define BTN_C 37
+#define M5_BUTTON_A 39
+#define M5_BUTTON_B 38
+#define M5_BUTTON_C 37
 
 #define MOTOR_RIGHT 16
 #define MOTOR_LEFT 17
@@ -21,38 +21,67 @@
 #define MOTOR_LEFT_CHANNEL 2
 #define RESOLUTION_BITS 10
 #define FREQUENCY 100
+
+enum RobotState {
+    FOLLOW, // Robot is following the line
+    SEARCH // Robot is searching for the line
+};
+
+// Screen - Refresh Rate
+#define REFRESH_DELAY 50
+unsigned long lastRefresh = 0;
+
+// Settings - MOTOR
+#define BASE_SPEED 512
+#define MIN_SPEED 230
+#define HARD_MIN_SPEED 50 // Used as a hard limit for the motors, to prevent them from stopping at all.
 #define MAX_SPEED 1023
-#define MIN_SPEED 0 // 230 - Temp 0
+#define SEARCH_SPEED 280
+#define RECOVER_SPEED 340
 
-// Settings
-#define BASE_SPEED 250 // Base Speed which is modified depending on the steering
-#define STEERING_SCALE 50 // Steering value scaled against this value
-#define DEAD_ZONE 0.05 // Correction dead zone, used to not correct if the steering error is too small
-#define CENTER_SENSOR_PENALTY 0.6 // Used to penalize the correction if the center sensor is on the line, prevents the bot from freaking out to much
+// Settings - Algorithm
+#define CORRECTION_STRENGTH 200
+#define DAMPING_STRENGTH 260
 
-// Reduces speeds at turns
-#define REDUCED_SPEED_THRESHOLD 0.20 // The steering value threshold at which the speed is reduced, this is used in case the turn is sharp and slower speeds should be used to lose the line
-#define SPEED_REDUCTION 0.50 // Speed reduction factor, used to reduce the speed
+int weights[SENSOR_COUNT] = {5, 1, 0, -1, -5};
 
-int reads[SENSOR_COUNT];
+// Settings - Search
+#define SEARCH_RAMP_START_MS 0
+#define SEARCH_RAMP_FULL_MS 900
+#define SEARCH_RAMP_MAX_BONUS 400
+#define SEARCH_BASE_TURN_BIAS 300
 
-float weights[5] = {-1, -0.5, 0, 0.5, 1};
+// Motor calibration
+#define MOTOR_LEFT_CORRECTION 0
+#define MOTOR_RIGHT_CORRECTION 0
+
+// Memory - General
+RobotState state = FOLLOW;
+int sensorValues[SENSOR_COUNT];
+
+// Memory - Follow
 float lastError = 0;
-int lostLineTime = 0;
+
+// Memory - Search
+unsigned long searchStartTimeStamp = 0;
+float lastSeenPosition = 0;
 
 void setup() {
     M5.begin();
 
-    // Define pins
-    pinMode(SENSOR_0, INPUT);
-    pinMode(SENSOR_1, INPUT);
-    pinMode(SENSOR_2, INPUT);
-    pinMode(SENSOR_3, INPUT);
-    pinMode(SENSOR_4, INPUT);
-    pinMode(BTN_A, INPUT);
-    pinMode(BTN_B, INPUT);
-    pinMode(BTN_C, INPUT);
+    // Sensors
+    pinMode(IR_L2, INPUT);
+    pinMode(IR_L1, INPUT);
+    pinMode(IR_C, INPUT);
+    pinMode(IR_R1, INPUT);
+    pinMode(IR_R2, INPUT);
 
+    // Buttons
+    pinMode(M5_BUTTON_A, INPUT);
+    pinMode(M5_BUTTON_B, INPUT);
+    pinMode(M5_BUTTON_C, INPUT);
+
+    // Motors
     ledcSetup(MOTOR_RIGHT_CHANNEL, FREQUENCY, RESOLUTION_BITS);
     ledcAttachPin(MOTOR_RIGHT, MOTOR_RIGHT_CHANNEL);
     ledcWrite(MOTOR_RIGHT_CHANNEL, 0);
@@ -64,7 +93,10 @@ void setup() {
     pinMode(INDICATOR, OUTPUT);
 }
 
-void debugDisplay(float steering, int speedLeft, int speedRight) {
+void debugDisplay(int speedRight, int speedLeft) {
+    if (millis() - lastRefresh < REFRESH_DELAY)
+        return;
+
     M5.Lcd.clear();
     M5.Lcd.setTextSize(2);
 
@@ -77,73 +109,153 @@ void debugDisplay(float steering, int speedLeft, int speedRight) {
 
     M5.Lcd.setCursor(97, 90);
     M5.Lcd.setTextSize(5);
-    M5.Lcd.printf("%.2f", steering);
+    M5.Lcd.printf("%.2f", lastSeenPosition);
 
     M5.Lcd.setTextSize(2);
+    M5.Lcd.setCursor(110, 150);
+    M5.Lcd.printf("State: %d", state);
+
     M5.Lcd.setCursor(10, 200);
 
     for (int i = 0; i < SENSOR_COUNT; i++) {
-        M5.Lcd.printf("S%d:%d ", SENSOR_COUNT - i, reads[SENSOR_COUNT - i - 1]);
+        M5.Lcd.printf("S%d:%d ", SENSOR_COUNT - i, sensorValues[i]);
+    }
+
+    lastRefresh = millis();
+}
+
+void move(int leftSpeed, int rightSpeed) {
+    leftSpeed = constrain(leftSpeed + MOTOR_LEFT_CORRECTION, 0, MAX_SPEED);
+    rightSpeed = constrain(rightSpeed + MOTOR_RIGHT_CORRECTION, 0, MAX_SPEED);
+
+    debugDisplay(rightSpeed, leftSpeed);
+
+    ledcWrite(MOTOR_LEFT_CHANNEL, leftSpeed);
+    ledcWrite(MOTOR_RIGHT_CHANNEL, rightSpeed);
+}
+
+void followLine(float error) {
+    digitalWrite(INDICATOR, HIGH); // Used as a visual indicator that the robot is following a line
+
+    float damping = error - lastError;
+    float correction = CORRECTION_STRENGTH * error + DAMPING_STRENGTH * damping;
+
+    // Ideal World Values - those are the values we want the motors to go.
+    int leftSpeed = BASE_SPEED - correction;
+    int rightSpeed = BASE_SPEED + correction;
+
+    if (leftSpeed > 0 && leftSpeed < MIN_SPEED) {
+        leftSpeed = MIN_SPEED - 20;
+        rightSpeed += 20;
+    }
+
+    if (rightSpeed > 0 && rightSpeed < MIN_SPEED) {
+        rightSpeed = MIN_SPEED - 20;
+        leftSpeed += 20;
+    }
+
+    rightSpeed = constrain(rightSpeed, HARD_MIN_SPEED, MAX_SPEED);
+    leftSpeed = constrain(leftSpeed, HARD_MIN_SPEED, MAX_SPEED);
+
+    move(leftSpeed, rightSpeed);
+    lastError = error;
+}
+
+int signOf(float value) {
+    if (value < 0) return -1;
+    if (value > 0) return 1;
+    return 0;
+}
+
+void search() {
+    digitalWrite(INDICATOR, LOW);
+
+    unsigned long elapsedTime = millis() - searchStartTimeStamp;
+
+    int direction = signOf(lastSeenPosition);
+    if (direction == 0) {
+        direction = signOf(lastError);
+        if (direction == 0) direction = 1;
+    }
+
+    int rampedBonus = 0;
+    if (elapsedTime > SEARCH_RAMP_START_MS) {
+        unsigned long rampTime = elapsedTime - SEARCH_RAMP_START_MS;
+        if (rampTime > (SEARCH_RAMP_FULL_MS - SEARCH_RAMP_START_MS)) {
+            rampTime = (SEARCH_RAMP_FULL_MS - SEARCH_RAMP_START_MS);
+        }
+
+        rampedBonus = (int)((float)rampTime / (float)(SEARCH_RAMP_FULL_MS - SEARCH_RAMP_START_MS) * SEARCH_RAMP_MAX_BONUS);
+    }
+
+    int turnBias = SEARCH_BASE_TURN_BIAS + rampedBonus;
+
+    int insideMotor = SEARCH_SPEED - turnBias;
+    int outsideMotor = SEARCH_SPEED + turnBias;
+
+    if (elapsedTime < SEARCH_RAMP_START_MS) {
+        insideMotor = SEARCH_SPEED;
+        outsideMotor = SEARCH_SPEED;
+    }
+
+    if (direction > 0) {
+        move(insideMotor, outsideMotor);
+    } else {
+        move(outsideMotor, insideMotor);
     }
 }
 
-void move(float steering) {
-    steering = constrain(steering, -1, 1);
+void readSensors(int &onLineSensors, float &position) {
+    onLineSensors = 0;
+    position = 0;
 
-    int left = (int) (BASE_SPEED + steering * STEERING_SCALE);
-    int right = (int) (BASE_SPEED - steering * STEERING_SCALE);
+    int weight = 0;
 
-    if (fabs(steering) > REDUCED_SPEED_THRESHOLD) {
-        if (steering < 0)
-            left = 0;
-        else
-            right = 0;
-    }
-
-    debugDisplay(steering, left, right);
-
-    ledcWrite(MOTOR_LEFT_CHANNEL, constrain(left, MIN_SPEED, MAX_SPEED));
-    ledcWrite(MOTOR_RIGHT_CHANNEL, constrain(right, MIN_SPEED, MAX_SPEED));
-}
-
-void loop() {
-    M5.update();
-
-    reads[0] = digitalRead(SENSOR_0);
-    reads[1] = digitalRead(SENSOR_1);
-    reads[2] = digitalRead(SENSOR_2);
-    reads[3] = digitalRead(SENSOR_3);
-    reads[4] = digitalRead(SENSOR_4);
-
-    float error = 0.0f;
-    int offLineSensors = 0;
+    sensorValues[0] = digitalRead(IR_L2);
+    sensorValues[1] = digitalRead(IR_L1);
+    sensorValues[2] = digitalRead(IR_C);
+    sensorValues[3] = digitalRead(IR_R1);
+    sensorValues[4] = digitalRead(IR_R2);
 
     for (int i = 0; i < SENSOR_COUNT; i++) {
-        if (reads[i] == HIGH) {
-            error += weights[i];
-            offLineSensors++;
+        bool onLine = !sensorValues[i];
+        if (onLine) {
+            onLineSensors++;
+            weight += weights[i];
         }
     }
 
-    bool centered = reads[2] == LOW;
-
-    if (offLineSensors == SENSOR_COUNT) {
-        error = lastError;
-        lostLineTime++;
+    if (onLineSensors > 0) {
+        position = (float) weight / (float) onLineSensors;
     } else {
-        if (offLineSensors > 0)
-            error /= offLineSensors;
+        position = 0;
+    }
+}
 
-        lastError = error;
-        lostLineTime = 0;
+void loop() {
+    int onLineSensors = 0;
+    float position = 0;
+
+    readSensors(onLineSensors, position);
+
+    if (onLineSensors == 0) {
+        if (state != SEARCH) {
+            state = SEARCH;
+            searchStartTimeStamp = millis();
+        }
+    } else {
+        lastSeenPosition = position;
+        state = FOLLOW;
     }
 
-    float steering = error * (lostLineTime / 20 + 1);
-    if (centered)
-        steering *= CENTER_SENSOR_PENALTY;
+    switch (state) {
+        case FOLLOW:
+            followLine(position);
+            break;
+        case SEARCH:
+            search();
+            break;
+    }
 
-    if (fabs(steering) < DEAD_ZONE)
-        steering = 0;
-
-    move(steering);
+    delay(5);
 }
